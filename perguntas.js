6e723d8ce2655def3a947f8777489105f3540177
alinecoghetto <aline.coghetto.martins@escola.pr.gov.br>
2025-08-21 22:29:08 -0300

    perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -13,3 +13,33 @@ criaCartao(
     'qual a tangente de 60°?',
     '= raiz de 3'
 )
+criaCartao(
+    'matematica',
+    'qual o seno de 30°?',
+    '= 1/2'
+)
+criaCartao(
+    'matematica',
+    'qual o seno de 45°?',
+    '= raiz de 2 sobre 2'
+)
+criaCartao(
+    'matematica',
+    'qual o seno de 60°?',
+    '= raiz de 3 sobre 2'
+)
+criaCartao(
+    'matematica',
+    'qual o coseno de 30°?',
+    '= raiz de 3 sobre 2'
+)
+criaCartao(
+    'matematica',
+    'qual o coseno de 45°?',
+    '= raiz de 2 sobre 2'
+)
+criaCartao(
+    'matematica',
+    'qual o coseno de 60°?',
+    '= 1/2'
+)
