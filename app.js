a969a8cafb8887ff4e6da6677ed238b1b4aeb068
alinecoghetto <aline.coghetto.martins@escola.pr.gov.br>
2025-08-21 22:12:28 -0300

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 function criaCartao( categoria, pergunta, resposta) {
-    console.log(categoria, pergunta, resposta);
-     
+  let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+
+    cartao.innerHTML=`
+    <div class="cartao__conteudo">
+                <h3>matemática</h3>
+                <div class="cartao__conteudo__pergunta">
+                        <p>qual o coseno de 60° ?</p>
+                </div>
+                <div class="cartao__conteudo__resposta">
+                       <p> = 1/2 </p>
+                </div>
+                </div>
+    `
+
+    container.appendChild(cartao)
 }
